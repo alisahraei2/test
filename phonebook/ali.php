3a48303d80d1848224dd4aad8f5dcee44df9e9cc
= <ali.sahraei110@gmail.com>
2017-02-03 14:25:39 +0330

    change syntax

--- phonebook/ali.php
+++ phonebook/ali.php
@@ -1,9 +1,12 @@
 <?php 
 $username = "ali";
 $password = 1234;
-if ($username == "ali" and $password == 123) {
+if ($username == "ali" and $password == 123) 
+{
 	echo "شما وارد شدید.";
-}else {
+}
+else 
+{
 	echo "نام کاربری یا کلمه عبور اشتباه است.";
 }
 ?>
